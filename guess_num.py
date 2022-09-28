1374e79fdf1fce1578cce268fb772d69d1e17a02
葉力瑜 <yehleeyu@yeliyude-MacBook-Air.local>
2022-09-28 19:08:13 +0100

    final version

--- guess_num.py
+++ guess_num.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1,100)
+start = input('請決定隨機數字範圍開始值')
+end =  input('請決定隨機數字結束值')
+start =int(start)
+end = int(end)
+r = random.randint(start,end)
 i = 0
 while True:
 	guess = input('1至100中，請猜一個數字：')
